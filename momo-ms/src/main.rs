de4aab654ac733c222d7e76c38aa45ddd4da72a4
quabynah-bilson <84394923+quabynah-bilson@users.noreply.github.com>
2022-11-15 13:53:59 +0000

    Update main.rs

--- momo-ms/src/main.rs
+++ momo-ms/src/main.rs
@@ -1,40 +1,22 @@
-// use actix_web::{App, HttpServer, middleware::Logger};
-//
-// use api::auth::login;
-//
-// mod api;
-//
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     // setup logger
-//     std::env::set_var("RUST_LOG", "debug");
-//     std::env::set_var("RUST_BACKTRACE", "1");
-//     env_logger::init();
-//
-//     // start server
-//     HttpServer::new(|| {
-//         // let logger = Logger::default();
-//         App::new()
-//             // .wrap(logger)
-//             .service(login);
-//     })
-//         .bind(("0.0.0.0", 9999))?
-//         .run()
-//         .await
-// }
+use actix_web::{App, HttpServer, middleware::Logger};
 
+use api::auth::login;
 
-use actix_web::{get, web, App, HttpServer, Responder};
-
-#[get("/hello")]
-async fn greet() -> impl Responder {
-    format!("Hello world!")
-}
+mod api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // setup logger
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    // setup server
     HttpServer::new(|| {
-        App::new().service(greet)
+        let logger = Logger::default();
+        App::new()
+            .wrap(logger)
+            .service(login)
     })
         .bind(("0.0.0.0", 9999))?
         .run()
